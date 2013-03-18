8e0509d6b93a24a163c583413c4d3aa4018fcc66
guertijp <jean-philippe.guertin-renaud@umontreal.ca>
2013-03-18 14:38:31 -0400

    Finalisation de JolisBoutons.

--- JolisBoutons/src/iro/ift2905/jolisboutons/JolisBoutonsActivity.java
+++ JolisBoutons/src/iro/ift2905/jolisboutons/JolisBoutonsActivity.java
@@ -15,8 +15,11 @@ import android.widget.Toast;
 /**
  * 
  * JolisBoutons est une introduction à la modification visuelle
- * des boutons. Différentes variations sont implantées, entre
- * autres texte seul, texte et image, case à cocher, etc.
+ * des boutons. Quatre variantes sont implantées :
+ * - Bouton avec texte simple
+ * - Bouton formaté en HTML
+ * - Bouton formé par un LinearLayout avec des TextView simples
+ * - Bouton formé par un LinearLayout avec des éléments avancés (image, texte, case à cocher)
  * 
  */
 
@@ -27,65 +30,92 @@ public class JolisBoutonsActivity extends Activity implements OnClickListener, O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        ////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////
-        //
-        // le bouton 1 est du texte simple.
-        //
+        /*
+         * Le bouton 1 est le plus simple. On se sert de la classe
+         * Button normale et le texte est directement défini dans
+         * le layout.
+         */
         Button b1 = (Button)findViewById(R.id.button1);
         b1.setOnClickListener(this);
         
-        ////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////
-        //
-        // le bouton 2 est un texte spécial...
-        // c'est du HTML simple
-        // ce genre de texte doit être défini dynamiquement
-        //
         
+        
+        /*
+         * Le bouton 2 est légèrement plus complexe. On ignore
+         * le texte du bouton tel que défini dans le layout
+         * et on génère à la place une séquence de texte
+         * dite "riche" (avec formatage) depuis du HTML
+         * simple.
+         */
         Button b2 = (Button)findViewById(R.id.button2);
         b2.setOnClickListener(this);
+        
+        
 
-        // on peut définir le texte comme suit...
+        /*
+         * La manière la plus simple d'insérer du texte
+         * par l'entremise de l'activité.
+         */
         b2.setText("Trop simple");
 
-        // on peut utilise du HTML directement
+        /*
+         * On peut directement insérer du code HTML
+         * à l'aide de Html.fromHtml. C'est ici qu'on
+         * peut remarquer que setText prend, non pas
+         * un objet String, mais bien un objet
+         * CharSequence, dont String n'est qu'une variante
+         * possible. À noter que les images ne sont
+         * pas immédiatement fonctionnelles et
+         * demandent à être remplacées ultérieurement,
+         * aussi est-il déconseillé de les utiliser
+         * de cette manière. Les balises HTML utilisables
+         * sont documentées ici :
+         * http://commonsware.com/blog/Android/2010/05/26/html-tags-supported-by-textview.html
+         */
         b2.setText(Html.fromHtml("<b>Bonjour</b><br>C'est <i>moi</i><br><img src=\"http://www.umontreal.ca/images/contenu/capsules/udem.png\">."));
 
-        // on peut aussi utiliser du HTML déposé dans une string (dans string.xml)
+        /*
+         * Finalement, la méthode standard pour gérer
+         * du texte en Android, les ressources. On peut
+         * stocker du code HTML dans une ressource
+         * à l'aide d'une syntaxe particulière
+         * (voir strings.xml).
+         */
         b2.setText(Html.fromHtml(getString(R.string.b2)));
         
-        //
-        // les tags qui sont utilisables sont documentés ici:
-        // http://commonsware.com/blog/Android/2010/05/26/html-tags-supported-by-textview.html
-        // (non, les images ne fonctionnent pas...)
-        //
-
-        ////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////
-        //
-        // Le bouton 3 est plus complexe (et plus flexible)
-        //
-        // >> Utilisez un ListView plutôt qu'un Button.
-        // >> Placez tous le composants (les TextView et autres) dans ce ListView
-        //
-        // >> Propriétés du ListView:  Clickable = True , Focusable = True
-        //
-
+        
+        
+        /*
+         * Le bouton 3 est en vérité un LinearLayout
+         * avec les propriétés Clickable et Focusable
+         * réglées à "true", ce qui en fait effectivement
+         * un bouton. En effet, on pourra remarquer que
+         * la méthode setOnClickListener, et donc toute la
+         * fonctionalité entourant le clic, fait partie
+         * de la classe View, la classe de base pour tous
+         * les éléments d'interface. Il est donc possible
+         * de transformer n'importe quel élément en bouton
+         * si désiré.
+         * 
+         * Dans ce cas-ci, on se contente de créer un
+         * LinearLayout contenant des TextViews et de l'utiliser
+         * comme un seul bouton monolithique.
+         */
         LinearLayout ly3 = (LinearLayout)findViewById(R.id.bouton3);
         ly3.setOnClickListener(this);
         
-        ////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////
-        //
-        // Le bouton 4 est comme le bouton 3, mais avec des OnClick différents
-        // pour certains éléments (le checkbox, essentiellement)
-        //
         
+        
+        /*
+         * Le bouton 4 est une variante légèrement plus avancée
+         * du bouton 3 où on a utilisé des éléments enfants
+         * plus complexes, par exemple une image ou une
+         * case à cocher. Tous ces éléments étant définis
+         * dans le layout principal, ils restent accessibles
+         * directement avec findViewById. On utilise cette
+         * propriété pour ajouter un événement sur la
+         * case à cocher.
+         */
         LinearLayout ly4 = (LinearLayout)findViewById(R.id.bouton4);
         ly4.setOnClickListener(this);
         
@@ -114,7 +144,6 @@ public class JolisBoutonsActivity extends Activity implements OnClickListener, O
 		Toast.makeText(this,info,Toast.LENGTH_SHORT).show();
 	}
 
-	// pour notre checkbox
 	@Override
 	public void onCheckedChanged(CompoundButton b, boolean val) {
 		Toast.makeText(JolisBoutonsActivity.this,"CHECK = "+val,Toast.LENGTH_SHORT).show();
